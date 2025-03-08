8677d9950de9d9d80322999208aff8693da78642
zeyad <zeyad@DESKTOP-C3N0M8E>
2025-03-08 03:54:11 +0200

    Department Module BBL

---
+++ Company_Abdelkader.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company_Abdelkader.DAL.Models;
+
+namespace Company_Abdelkader.BLL.Interfaces
+{
+    internal interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll();
+        Department GetById(int id);
+
+        int Add(Department model);
+        int Update(Department model);
+        int Delete(Department model);
+
+    }
+}
---
+++ Company_Abdelkader.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company_Abdelkader.BLL.Interfaces;
+using Company_Abdelkader.DAL.Data.Contexts;
+using Company_Abdelkader.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Company_Abdelkader.BLL.Repositories
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly CompanyDbContext _context;
+
+        public DepartmentRepository()
+        {
+            _context = new CompanyDbContext();
+        }
+        public IEnumerable<Department> GetAll()
+        {
+            
+            return _context.Departments.ToList();
+        }
+
+        public Department GetById(int id)
+        {
+            
+            return _context.Departments.Find(id);
+        }
+
+        public int Add(Department model)
+        {
+          
+             _context.Departments.Add(model);
+            return _context.SaveChanges();
+        }
+
+        public int Update(Department model)
+        {
+
+            _context.Departments.Update(model);
+            return _context.SaveChanges();
+        }
+
+        public int Delete(Department model)
+        {
+            
+            _context.Departments.Remove(model);
+            return _context.SaveChanges();
+        }
+
+      
+
+        
+
+       
+    }
+}
