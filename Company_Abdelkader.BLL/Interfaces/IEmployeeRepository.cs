537a10ab56706561e40246659c0e7e6cae2c0049
zeyad <zeyad@DESKTOP-C3N0M8E>
2025-03-14 03:15:55 +0200

    Implementation BLL of Employee

---
+++ Company_Abdelkader.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company_Abdelkader.DAL.Models;
+
+namespace Company_Abdelkader.BLL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee GetById(int id);
+
+        int Add(Employee model);
+        int Update(Employee model);
+        int Delete(Employee model);
+
+
+    }
+}
